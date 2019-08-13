a208259be21b72a88aca83de11710be2ce7baaf5
Pete Frost <pete@coinvestor.co.uk>
2019-08-13 17:11:23 +0100

    register queue connector

--- src/BatchSQSProvider.php
+++ src/BatchSQSProvider.php
@@ -1,12 +1,16 @@
 <?php
 namespace CoInvestor\BatchSQSQueue;
 
-
+use Illuminate\Support\ServiceProvider;
+use CoInvestor\BatchSQSQueue\Queues\Connectors\SqsConnector;
 
 class BatchSQSProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->app["queue"]->addConnector("batch-sqs", function () {
+            return new SqsConnector;
+        });
     }
 
     public function register()
