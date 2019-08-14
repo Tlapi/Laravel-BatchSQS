e2fb8de6116564fb5a8201f8f3ab1edff983a051
Pete Frost <pete@coinvestor.co.uk>
2019-08-14 09:21:42 +0100

    add linter config & fix some formatting

--- src/BatchSQSProvider.php
+++ src/BatchSQSProvider.php
@@ -9,7 +9,7 @@ class BatchSQSProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'config/batch-sqs.php' => config_path('batch-sqs.php'),
+            __DIR__ . 'config/batch-sqs.php' => config_path('batch-sqs.php'),
         ]);
         $this->app["queue"]->addConnector("batch-sqs", function () {
             return new SqsConnector;
